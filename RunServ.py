410504b8df291413c76e4cce78a787761478aea9
yasminebenkacir <49860169+yasminejaz@users.noreply.github.com>
2021-01-27 16:39:46 -0100

    Add files via upload

---
+++ RunServ.py
@@ -0,0 +1,24 @@
+import win32con
+import win32service
+
+
+def ListServices():
+
+    accessSCM = win32con.GENERIC_READ
+    accessSrv = win32service.SC_MANAGER_ALL_ACCESS
+
+    # Open Service Control Manager
+    hscm = win32service.OpenSCManager(None, None, accessSCM)
+
+    # Enumerate Service Control Manager DB
+    typeFilter = win32service.SERVICE_WIN32_OWN_PROCESS(startMode='auto')
+    stateFilter = win32service.SERVICE_STATE_ALL
+
+    statuses = win32service.EnumServicesStatus(hscm, typeFilter, stateFilter)
+
+    for (short_name, desc, status) in statuses:
+        # if status[1]==4:
+            print(short_name, status)
+
+
+ListServices()
