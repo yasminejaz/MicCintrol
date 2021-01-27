1b4b2ebed83c80fe8747e2842a870f29035332fc
yasminebenkacir <49860169+yasminejaz@users.noreply.github.com>
2021-01-27 16:41:07 -0100

    Update RunServ.py

--- RunServ.py
+++ RunServ.py
@@ -11,13 +11,13 @@ def ListServices():
     hscm = win32service.OpenSCManager(None, None, accessSCM)
 
     # Enumerate Service Control Manager DB
-    typeFilter = win32service.SERVICE_WIN32_OWN_PROCESS(startMode='auto')
+    typeFilter = win32service.SERVICE_WIN32_OWN_PROCESS # Le type de service de notre fake service
     stateFilter = win32service.SERVICE_STATE_ALL
 
     statuses = win32service.EnumServicesStatus(hscm, typeFilter, stateFilter)
 
     for (short_name, desc, status) in statuses:
-        # if status[1]==4:
+        if status[1]==4: # if running 
             print(short_name, status)
 
 
